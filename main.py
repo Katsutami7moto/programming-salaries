6cd5ad266e54341f2a0a948c14cdb494b9394ec8
Katsutami7moto <nclsf87@gmail.com>
2022-07-07 20:11:46 +0300

    Count average predicted salaries by language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pprint
-import statistics
+import json
+from statistics import mean
 
 import requests
 
@@ -14,7 +14,7 @@ def get_vacancies(lang: str):
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    return response.json()['items']
+    return response.json()
 
 
 def predict_rub_salary(vacancy: dict):
@@ -25,29 +25,11 @@ def predict_rub_salary(vacancy: dict):
     if salary_currency != 'RUR':
         return None
     if salary_from and salary_to:
-        return statistics.mean([salary_from, salary_to])
+        return mean([salary_from, salary_to])
     if not salary_to:
         return salary_from * 1.2
     if not salary_from:
         return salary_to * 0.8
-
-
-def show_predicted_rub_salaries(lang: str):
-    for vacancy in get_vacancies(lang):
-        print(predict_rub_salary(vacancy))
-
-
-def get_jobs_number(lang: str):
-    url = 'https://api.hh.ru/vacancies'
-    payload = {
-        'text': f'программист {lang}',
-        'area': 1,
-        'period': 30,
-        'only_with_salary': True,
-    }
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response.json()['found']
 
 
 def main():
@@ -63,12 +45,24 @@ def main():
         'Swift',
         'Scala',
     ]
-    jobs_numbers = dict()
+    langs_jobs = dict()
     for lang in prog_langs:
-        jobs_numbers[lang] = get_jobs_number(lang)
-    pprint.pprint(jobs_numbers, indent=4)
+        raw_lang_jobs = get_vacancies(lang)
+        lang_jobs = raw_lang_jobs['items']
+        lang_jobs_salaries = tuple(
+            filter(
+                lambda x: x is not None,
+                map(predict_rub_salary, lang_jobs)
+            )
+        )
+        jobs_avg_salary = {
+            "vacancies_found": raw_lang_jobs['found'],
+            "vacancies_processed": len(lang_jobs_salaries),
+            "average_salary": int(mean(lang_jobs_salaries)),
+        }
+        langs_jobs[lang] = jobs_avg_salary
+    print(json.dumps(langs_jobs, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     main()
-    show_predicted_rub_salaries('Python')
